87d1ded1dcfd141cca53216647a0cdb4300d3279
aryasuva2250 <as3526@njit.edu>
2021-08-02 20:37:16 -0400

    Step 5: Configuring Your Flask App

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 """Initialize Flask Application."""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, template_folder="templates")
+    app.config.from_pyfile('config.py')
 
     with app.app_context():
         from . import routes
-
+        db.create_all()
         return app
