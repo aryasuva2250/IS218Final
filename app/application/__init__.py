0056039807908f2a1d741b3fe646b9853e47bd31
aryasuva2250 <as3526@njit.edu>
2021-08-02 21:03:41 -0400

    Step 7: Organizing Flask Apps with Blueprints

--- app/application/__init__.py
+++ app/application/__init__.py
@@ -1,18 +1,17 @@
+"""Initialize Flask app."""
 from flask import Flask
-from flaskext.mysql import MySQL
-from pymysql.cursors import DictCursor
 
-mysql = MySQL(cursorclass=DictCursor)
 
-def init_app():
-    """Initialize the core application."""
-    app = Flask(__name__)
-    app.config.from_pyfile('config.py')
-    mysql.init_app(app)
+def create_app():
+    """Create Flask application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
 
     with app.app_context():
-        
-        from .home.routes import home_bp
-        app.register_blueprint(home_bp)
+        # Import parts of our application
+        from .home import routes
+
+        # Register Blueprints
+        app.register_blueprint(home.home_bp)
 
         return app
